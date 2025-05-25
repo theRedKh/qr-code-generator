2e640767661aea6b1e2657c38469f3501aa97eca
Khadeeja H <147099250+theRedKh@users.noreply.github.com>
2025-05-25 14:14:04 -0400

    add security - prevent cross site scripting, add color picker - customizable qr code generation

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 function App() {
   const [imageURL, setImageUrl] = useState(null);
   const [inputText, setInputText] = useState('');
+  const [fillColor, setFillColor] = useState('#000000'); // default black
+  const [bgColor, setBgColor] = useState('#FFFFFF'); // default white
+
   const handleGenerate = async () => {
     try {
       const response = await fetch('http://localhost:5000/generate', {
@@ -11,8 +14,20 @@ function App() {
         headers: {
           'Content-Type': 'application/json', //sending JSON data to server
         },
-        body: JSON.stringify({ text: inputText }), //convert input to JSON string
+        body: JSON.stringify({ 
+          data: inputText, //convert input to JSON string
+          fill_color: fillColor, //convert input to JSON string
+          back_color: bgColor //convert input to JSON string
+        }), 
       });
+
+      if (!response.ok) {
+        // status code is not in the range 200-299
+        const errorData = await response.json();
+        alert("Error: " + errorData.error);
+        return; //stop any further execution
+      }
+      // response is ok, proceed to get the image
 
       const blob = await response.blob(); //convert raw data to Binary Large Object
       const url = window.URL.createObjectURL(blob); //temp URL for image blob
@@ -22,6 +37,7 @@ function App() {
       console.error('Error generating QR code:', error);      
     }
   };
+
   return (
     <>
       <div>
@@ -36,6 +52,20 @@ function App() {
           placeholder="Enter text here" 
         />
         <button onClick={handleGenerate}>Generate</button>
+      </div>
+      <div>
+        <input 
+          type="color"
+          value={fillColor}
+          onChange={(e) => setFillColor(e.target.value)}
+          title="Select fill color (QR code Dots)"
+        />
+        <input
+          type="color" 
+          value={bgColor}
+          onChange={(e) => setBgColor(e.target.value)}
+          title="Select background color (QR code Background)"
+        />
       </div>
       {imageURL && <img src={imageURL} alt="Generated QR Code" />}
     </>
--- qrCodeBackend.py
+++ qrCodeBackend.py
@@ -5,6 +5,8 @@ import qrcode #generate qr code
 import io #handle image data in memory instead of directly saving to disk
 from PIL import Image #work with images
 
+import re #regular expressions library - specific patterns in strings - using for XSS check
+
 #Create new Flask app
 app = Flask(__name__)
 CORS(app) #enable for all routes
@@ -13,17 +15,35 @@ CORS(app) #enable for all routes
 
 #Generate QR code button function
 def generate_qr():
-    data = request.json.get("text", "").strip() #grabs JSON from frontend
+    data = request.json.get("data", "").strip() #grabs JSON from frontend
+    fill_color = request.json.get("fill_color", "black")
+    back_color = request.json.get("back_color", "white")
+
     if not data:
         return jsonify({"error": "No text provided"}), 400 #check if user forgot to send text, HTTP status 400 error
+    
+    if not is_safe_text(data):
+        return jsonify({"error": "Insecure content detected"}), 400 #bad request for insecure input data
+    
     qr = qrcode.QRCode(version=1, box_size=5, border=5)
     qr.add_data(data)
     qr.make() #finalize layout
-    img = qr.make_image(fill_color="black", back_color="white") #black and white PNG
+    img = qr.make_image(fill_color=fill_color, back_color=back_color) #black and white PNG
     buf = io.BytesIO() #in memory file creation
     img.save(buf, format="PNG")
     buf.seek(0)  #rewind memory buffer to beginning so Flask can see it
+    
     return send_file(buf, mimetype="image/png") #flask sends image back as a response
+
+def is_safe_text(text):
+    #no script tags or javascript URLS
+    if re.search(r'<script.*?>', text, re.IGNORECASE):
+        return False
+    
+    if text.lower().startswith('javascript: '):
+        return False
+    
+    return True
 
 if __name__ == "__main__":
     app.run(debug=True) #run flask app only if script is run directly
